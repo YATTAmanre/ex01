5f878b95b5076a3f6b78716486ba2a2d2f602baa
SUZUKI Haruto <s1300056.u-aizu.ac.jp>
2023-06-09 12:14:30 +0900

    Dice game: initial revision

---
+++ DiceGame.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+  int d1,d2;
+  printf("Rolling dice...\n");
+  d1 = rand()%6+1;
+  d2 = rand()%6+1;
+  printf("Die 1: %d\n",d1);
+  printf("Die 2: %d\n",d2);
+  printf("Total value: %d\n", d1+d2);
+
+  return 0;
+}
+  
